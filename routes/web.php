c3ba3004508c1459e75f4012b30edb26098f9806
Md Siddiqur Rahman <53170094+siddik-web@users.noreply.github.com>
2024-01-13 14:22:28 +0600

    add expenses index view and route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/expenses', [ExpenseController::class, 'index'])->name('expense.list');
