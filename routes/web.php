78e3cdd3e8dd6875c40b450e8cf5fca5a927c911
Md Siddiqur Rahman <53170094+siddik-web@users.noreply.github.com>
2024-01-13 16:20:23 +0600

    update  route group

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => ['auth']], function () {
     Route::get('/expenses', [ExpenseController::class, 'index'])->name('expense.list');
 });
 
